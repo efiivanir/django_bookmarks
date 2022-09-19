8f1843a9cd0f3d9e6b65af3da3309e83ae6513f2
Ivanir Efi <efi.ivanir@gmail.com>
2022-09-19 10:52:15 +0300

    Ch04 page 174 Before User registration and user profiles

--- account/urls.py
+++ account/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from . import views
 urlpatterns = [
@@ -14,6 +14,22 @@ urlpatterns = [
     path('password-change/done/',
         auth_views.PasswordChangeDoneView.as_view(),
         name='password_change_done'),
+
+    # reset password urls
+    # path('password-reset/',
+    #     auth_views.PasswordResetView.as_view(),
+    #     name='password_reset'),
+    # path('password-reset/done/',
+    #     auth_views.PasswordResetDoneView.as_view(),
+    #     name='password_reset_done'),
+    # path('password-reset/<uidb64>/<token>/',
+    #     auth_views.PasswordResetConfirmView.as_view(),
+    #     name='password_reset_confirm'),
+    # path('password-reset/complete/',
+    #     auth_views.PasswordResetCompleteView.as_view(),
+    #     name='password_reset_complete'),
+
+    path('', include('django.contrib.auth.urls')),
 
     path('', views.dashboard, name='dashboard'),
 ]
